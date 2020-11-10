83d38a5bf6a9abbe50b5a32f564195489036d00e
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2020-11-10 13:25:05 +0530

    Create links.js

---
+++ links.js
@@ -0,0 +1,52 @@
+  
+  async function makefdl(key, st, si, sd, domain, link){
+	  
+  var  bodydata = JSON.stringify({
+	"longDynamicLink": "https://"+ domain + "/?link=" + link + "&st=" + st + "&sd="+ sd + "&si=" + si,
+	"suffix": {
+		"option": "SHORT"
+	}
+});
+
+var url = "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=" + key;
+    
+    
+     
+     var xhr = new XMLHttpRequest();
+xhr.open('POST', url , true);
+xhr.setRequestHeader('Content-type', 'application/json');
+xhr.onload = function () {
+	
+    var data = JSON.parse(this.responseText);
+    var stlink = data.shortLink;
+    return stlink;
+};
+xhr.send(bodydata);
+     
+}
+
+  
+  async function makedevfdl(key, st, si, sd, domain, link){
+	  
+  var  bodydata = JSON.stringify({
+	"longDynamicLink": "https://"+ domain + "/?link=" + link + "&st=" + st + "&sd="+ sd + "&si=" + si,
+	"suffix": {
+		"option": "SHORT"
+	}
+});
+
+var url = "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=" + key;
+    
+    
+     
+     var xhr = new XMLHttpRequest();
+xhr.open('POST', url , true);
+xhr.setRequestHeader('Content-type', 'application/json');
+xhr.onload = function () {
+	
+    var data = JSON.parse(this.responseText);
+    return data;
+};
+xhr.send(bodydata);
+     
+}
